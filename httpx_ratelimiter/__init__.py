04d3c74b25eaa9a60b2cd30005f900474a8890a5
Anatoly Raev <cralixraev@gmail.com>
2023-10-16 13:48:34 +0300

    update docs, new version

--- httpx_ratelimiter/__init__.py
+++ httpx_ratelimiter/__init__.py
@@ -1,6 +1,5 @@
-"""Simple wrapper around PyrateLimiter based on Requests-ratelimiter
-that adds integration with httpx library"""
-__version__ = "0.0.2"
+"""Ratelimiting for httpx"""
+__version__ = "0.0.3"
 
 # ruff: noqa: F401,F403,F405
 from pyrate_limiter import *
